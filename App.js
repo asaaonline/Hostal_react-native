a7494f884109f8d4628d89c567b025c8be128304
asela <asaaonline@users.noreply.github.com>
2018-09-17 22:28:04 +0530

    first

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import LoginScreen from './src/login/login'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <LoginScreen />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
